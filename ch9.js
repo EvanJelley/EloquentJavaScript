1d0fa8898bef76bfda1378cce9887e3c165510ac
Evan Jelley <jelleyevan@gmail.com>
2024-02-23 13:01:47 -0700

    ch

---
+++ ch9.js
@@ -0,0 +1,18 @@
+function getDate(string) {
+    let [_, month, day, year] =
+        /(\d{1,2})-(\d{1,2})-(\d{4})/.exec(string);
+    return new Date(year, month - 1, day);
+}
+//   console.log(getDate("1-30-2003"));
+
+let stock = "1 lemon, 2 cabbages, and 101 eggs";
+function minusOne(match, amount, unit) {
+    amount = Number(amount) - 1;
+    if (amount == 1) { // only one left, remove the 's'
+        unit = unit.slice(0, unit.length - 1);
+    } else if (amount == 0) {
+        amount = "no";
+    }
+    return amount + " " + unit;
+}
+console.log(stock.replace(/(\d+) (\w+)/g, minusOne));
