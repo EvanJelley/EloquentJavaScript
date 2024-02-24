b6edebcc5543d71f866fde9aa55337da80016631
Evan Jelley <jelleyevan@gmail.com>
2024-02-24 13:48:59 -0700

    Update ch9.js

--- ch9.js
+++ ch9.js
@@ -15,4 +15,70 @@ function minusOne(match, amount, unit) {
     }
     return amount + " " + unit;
 }
-console.log(stock.replace(/(\d+) (\w+)/g, minusOne));
+
+// Exercise 1
+
+verify(/ca(t|r)/,
+    ["my car", "bad cats"],
+    ["camper", "high art"]);
+
+verify(/pr?op/,
+    ["pop culture", "mad props"],
+    ["plop", "prrrop"]);
+
+verify(/ferr(et|y|ari)/,
+    ["ferret", "ferry", "ferrari"],
+    ["ferrum", "transfer A"]);
+
+verify(/\w+ious\b/,
+    ["how delicious", "spacious room"],
+    ["ruinous", "consciousness"]);
+
+verify(/\s[^\w]/,
+    ["bad punctuation ."],
+    ["escape the period"]);
+
+verify(/\w{6,}/,
+    ["Siebentausenddreihundertzweiundzwanzig"],
+    ["no", "three small words"]);
+
+verify(/\b[a-df-z]+\b/i,
+    ["red platypus", "wobbling nest"],
+    ["earth bed", "learning ape", "BEET"]);
+
+
+function verify(regexp, yes, no) {
+    // Ignore unfinished exercises
+    if (regexp.source == "...") return;
+    for (let str of yes) if (!regexp.test(str)) {
+        console.log(`Failure to match '${str}'`);
+    }
+    for (let str of no) if (regexp.test(str)) {
+        console.log(`Unexpected match for '${str}'`);
+    }
+}
+
+// Exercise 2
+
+let text = "'I'm the cook,' he said, 'it's my job.'";
+// Change this call.
+console.log(text.replace(/(\W)'|'(\W)|(^)'|'($)/g, "$1\""));
+// → "I'm the cook," he said, "it's my job."
+
+// Exercise 3
+// Fill in this regular expression.
+let number = /^(\+|\-)?\d*(.\.|\..)?\d*(e(\+|\-)?)?\d*$/i;
+
+// Tests:
+for (let str of ["1", "-1", "+15", "1.55", ".5", "5.",
+    "1.3e2", "1E-4", "1e+12"]) {
+    if (!number.test(str)) {
+        console.log(`Failed to match '${str}'`);
+    }
+}
+for (let str of ["1a", "+-1", "1.2.3", "1+1", "1e4.5",
+    ".5.", "1f5", "."]) {
+    if (number.test(str)) {
+        console.log(`Incorrectly accepted '${str}'`);
+    }
+}
