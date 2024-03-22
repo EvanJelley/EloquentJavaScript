a5ad52d4d2c16df5cb8b4317f5ae05aa86b18025
Evan Jelley <jelleyevan@gmail.com>
2024-03-22 13:25:00 -0600

    ch18 work

--- ch18.js
+++ ch18.js
@@ -5,3 +5,30 @@ fetch("https://www.wikipedia.org/").then(response => {
     console.log(response.headers.get("Content-Type"));
     response.text().then(text => console.log(text));
 })
+
+
+Exercise 1
+fetch("https://eloquentjavascript.net/author", {headers: {Accept: "text/plain"}}).then(response => {
+    console.log(response.status);
+    console.log(response.headers.get("Content-Type"));
+    response.text().then(text => console.log(text));
+});
+
+fetch("https://eloquentjavascript.net/author", {headers: {Accept: "text/html"}}).then(response => {
+    console.log(response.status);
+    console.log(response.headers.get("Content-Type"));
+    response.text().then(text => console.log(text));
+});
+
+fetch("https://eloquentjavascript.net/author", {headers: {Accept: "application/json"}}).then(response => {
+    console.log(response.status);
+    console.log(response.headers.get("Content-Type"));
+    response.text().then(text => console.log(text));
+});
+
+
+fetch("https://eloquentjavascript.net/author", {headers: {Accept: "application/rainbows+unicorns"}}).then(response => {
+    console.log(response.status);
+    console.log(response.headers.get("Content-Type"));
+    response.text().then(text => console.log(text));
+})
