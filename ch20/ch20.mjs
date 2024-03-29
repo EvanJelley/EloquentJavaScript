282d0cfc5b150b71e560ef116f1f30f071fcd21b
Evan Jelley <jelleyevan@gmail.com>
2024-03-29 13:37:13 -0600

    ch20 work

--- ch20/ch20.mjs
+++ ch20/ch20.mjs
@@ -1,5 +1,30 @@
-import {reverse} from './reverse.mjs';
+import { reverse } from './reverse.mjs';
+import { createWriteStream, readFile, writeFile } from 'node:fs';
+import { join } from 'node:path';
+import { createServer, request } from 'node:http';
 
-let argrument = process.argv[2];
+// readFile('/Users/evanjelley/Documents/CS/EloquentJavaScript/ch20/file.txt',
+//     "utf8", (error, text) => {
+//         if (error) throw error;
+//         console.log(text);
+//     });
+//
 
-console.log(reverse(argrument));
+// writeFile('/Users/evanjelley/Documents/CS/EloquentJavaScript/ch20/file.txt',
+//     "I am writing new text to this file",
+//     (err) => {
+//         if (err) throw err;
+//         console.log('File written');
+//     });
+
+// let txtFileStream = createWriteStream('/Users/evanjelley/Documents/CS/EloquentJavaScript/ch20/file.txt', { flags: 'a' });
+// txtFileStream.write('I am writing new text to this file\n');
+
+// createServer((request, response) => {
+//     response.writeHead(200, { 'Content-Type': 'text/plain' });
+//     request.on('data', chunk => 
+//     response.write(chunk.toString().toUpperCase()));
+//     request.on('end', () => response.end());
+// }).listen(8000);
+// console.log('Server running on port 8000');
+
