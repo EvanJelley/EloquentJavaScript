362f168c6027c5da295fb88365842c552a94b677
Evan Jelley <jelleyevan@gmail.com>
2024-03-30 13:40:07 -0600

    ch20 work

--- ch20/ch20.mjs
+++ ch20/ch20.mjs
@@ -1,30 +1,91 @@
 import { reverse } from './reverse.mjs';
-import { createWriteStream, readFile, writeFile } from 'node:fs';
+import { createWriteStream, readFile, writeFile, createReadStream } from 'node:fs';
 import { join } from 'node:path';
 import { createServer, request } from 'node:http';
+import { parse } from 'node:url';
+import { resolve, sep } from 'node:path';
+import { stat, readdir, rmdir, unlink } from 'node:fs/promises';
+import mime from 'mime';
 
-// readFile('/Users/evanjelley/Documents/CS/EloquentJavaScript/ch20/file.txt',
-//     "utf8", (error, text) => {
-//         if (error) throw error;
-//         console.log(text);
-//     });
-//
+const methods = Object.create(null);
 
-// writeFile('/Users/evanjelley/Documents/CS/EloquentJavaScript/ch20/file.txt',
-//     "I am writing new text to this file",
-//     (err) => {
-//         if (err) throw err;
-//         console.log('File written');
-//     });
+createServer((request, response) => {
+    let handler = methods[request.method] || notAllowed;
+    handler(request)
+        .catch(error => {
+            if (error.status != null) return error;
+            return { body: String(error), status: 500 };
+        })
+        .then(({ body, status = 200, type = 'text/plain' }) => {
+            response.writeHead(status, { 'Content-Type': type });
+            if (body && body.pipe) body.pipe(response);
+            else response.end(body);
+        });
+}).listen(8000);
 
-// let txtFileStream = createWriteStream('/Users/evanjelley/Documents/CS/EloquentJavaScript/ch20/file.txt', { flags: 'a' });
-// txtFileStream.write('I am writing new text to this file\n');
+async function notAllowed(request) {
+    return {
+        status: 405,
+        body: `Method ${request.method} not allowed.`
+    };
+};
 
-// createServer((request, response) => {
-//     response.writeHead(200, { 'Content-Type': 'text/plain' });
-//     request.on('data', chunk => 
-//     response.write(chunk.toString().toUpperCase()));
-//     request.on('end', () => response.end());
-// }).listen(8000);
-// console.log('Server running on port 8000');
+const baseDirectory = process.cwd();
 
+function urlPath(url) {
+    let { pathname } = new URL(url, "http://d");
+    let path = resolve(decodeURIComponent(pathname).slice(1));
+    if (path != baseDirectory &&
+        !path.startsWith(baseDirectory + sep)) {
+        throw { status: 403, body: 'Forbidden' };
+    }
+    return path;
+}
+
+methods.GET = async function (request) {
+    let path = urlPath(request.url);
+    let stats;
+    try {
+        stats = await stat(path);
+    } catch (error) {
+        if (error.code != 'ENOENT') throw error;
+        else return { status: 404, body: 'File not found' };
+    }
+    if (stats.isDirectory()) {
+        return { body: (await readdir(path)).join('\n') };
+    } else {
+        return {
+            body: createReadStream(path),
+            type: mime.getType(path)
+        };
+    }
+}
+
+methods.DELETE = async function (request) {
+    let path = urlPath(request.url);
+    let stats;
+    try {
+        stats = await stat(path);
+    } catch (error) {
+        if (error.code != 'ENOENT') throw error;
+        else return { status: 204 };
+    }
+    if (stats.isDirectory()) await rmdir(path);
+    else await unlink(path);
+    return { status: 204 };
+}
+
+function pipeStream(from, to) {
+    return new Promise((resolve, reject) => {
+        from.on("error", reject);
+        to.on("error", reject);
+        to.on("finish", resolve);
+        from.pipe(to);
+    });
+}
+
+methods.PUT = async function (request) {
+    let path = urlPath(request.url);
+    await pipeStream(request, createWriteStream(path));
+    return { status: 204 };
+};
